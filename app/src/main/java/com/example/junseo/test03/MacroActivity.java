6f8115816ce7b10ada741e430f42d915632d27bd
riosutenheat <l035833@naver.com>
2017-07-31 17:40:28 +0900

    // change 0731 Macro Update

--- app/src/main/java/com/example/junseo/test03/MacroActivity.java
+++ app/src/main/java/com/example/junseo/test03/MacroActivity.java
@@ -1,103 +1,234 @@
 package com.example.junseo.test03;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
+import java.util.List;
 
-public class MacroActivity extends AppCompatActivity implements View.OnClickListener
-{
+public class MacroActivity extends AppCompatActivity {
+    public static final String ARTIST_NAME = "com.example.junseo.test03.artistname";
+    public static final String ARTIST_ID = "com.example.junseo.test03.artistid";
 
-    Button btnSave;
-    Button btnGoBack;
+    EditText editTextName;
+    Spinner spinnerGenre;
+    Button buttonAddArtist;
+    ListView listViewArtists;
 
-    EditText NewName;
+    //a list to store all the artist from firebase database
+    List<macro> artists;
 
-    ListView lvListview;
-
-    static int Number;
-
-    static ArrayList<String> Namelists;
-
-    SharedPreferences preferences;
-    SharedPreferences preference;
+    //our database reference object
+    DatabaseReference databaseArtists;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_macro);
 
-        preferences = getSharedPreferences("Name_List", Context.MODE_PRIVATE);
-        preference = getSharedPreferences("Login", Context.MODE_PRIVATE);
+        //getting the reference of artists node
+        databaseArtists = FirebaseDatabase.getInstance().getReference("artists");
 
-        btnGoBack= (Button) findViewById(R.id.btnGoBack);
-        btnSave = (Button) findViewById(R.id.btnSave);
+        //getting views
+        editTextName = (EditText) findViewById(R.id.editTextName);
 
-        NewName= (EditText) findViewById(R.id.etNewName);
+        listViewArtists = (ListView) findViewById(R.id.listViewArtists);
+        buttonAddArtist = (Button) findViewById(R.id.buttonAddArtist);
 
-        lvListview= (ListView) findViewById(R.id.lvListview);
+        //list to store artists
+        artists = new ArrayList<>();
 
-        btnSave.setOnClickListener(this);
-        btnGoBack.setOnClickListener(this);
 
-        GettingDataintoArrayList();
-        PrintingData();
+        //adding an onclicklistener to button
+        buttonAddArtist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //calling the method addArtist()
+                //the method is defined below
+                //this method is actually performing the write operation
+                addArtist();
+            }
+        });
+
+        //리스트 뷰 아이템 접근 (여기서 모듈 전송)
+        listViewArtists.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //getting the selected artist
+                macro artist = artists.get(i);
+
+                //creating an intent
+             //   Intent intent = new Intent(getApplicationContext(), Activity.class);
+
+                //putting artist name and id to intent
+              //  intent.putExtra(ARTIST_ID, artist.getArtistId());
+              //  intent.putExtra(ARTIST_NAME, artist.getArtistName());
+
+                //리스트 아이템 눌렀을 때 Intent
+                //startActivity(intent);
+            }
+        });
+        //꾹 눌렀을 때 실행하기
+        listViewArtists.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                macro artist = artists.get(i);
+                showUpdateDeleteDialog(artist.getArtistId(), artist.getArtistName());
+                return true;
+            }
+        });
+    }
+    // 수정 혹은 삭제
+    private void showUpdateDeleteDialog(final String artistId, String artistName) {
+
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        final View dialogView = inflater.inflate(R.layout.update_dialog, null);
+        dialogBuilder.setView(dialogView);
+
+        final EditText editTextName = (EditText) dialogView.findViewById(R.id.editTextName);
+
+        final Button buttonUpdate = (Button) dialogView.findViewById(R.id.buttonUpdateArtist);
+        final Button buttonDelete = (Button) dialogView.findViewById(R.id.buttonDeleteArtist);
+
+        dialogBuilder.setTitle(artistName);
+        final AlertDialog b = dialogBuilder.create();
+        b.show();
+
+        //갱신 버튼
+        buttonUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = editTextName.getText().toString().trim();
+              //  String genre = spinnerGenre.getSelectedItem().toString();
+                if (!TextUtils.isEmpty(name)) {
+                    updateArtist(artistId, name);
+                    b.dismiss();
+                }
+            }
+        });
+
+        //삭제
+        buttonDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                deleteArtist(artistId);
+                b.dismiss();
+            }
+        });
     }
 
-    private void GettingDataintoArrayList() {
-        Number = preference.getInt("Number", 0);
+    private boolean updateArtist(String id, String name) {
+        //getting the specified artist reference
+        DatabaseReference dR = FirebaseDatabase.getInstance().getReference("artists").child(id);
 
-        Namelists = new ArrayList<String>();
-        for (int i = 0; i < Number; i++) {
-            String Name = preferences.getString("Name " + i, null);
-            Namelists.add(Name);
-        }
+        //updating artist
+        macro artist = new macro(id, name);
+        dR.setValue(artist);
+        Toast.makeText(getApplicationContext(), "상용구가 수정되었습니다.", Toast.LENGTH_LONG).show();
+        return true;
     }
 
-    private void StoringDataintoSharedfile() {
-        SharedPreferences.Editor editor = preferences.edit();
-        for (int i = 0; i < Namelists.size(); i++) {
-            editor.putString("Name " + i, Namelists.get(i));
-        }
-        editor.commit();
-    }
+    private boolean deleteArtist(String id) {
+        //getting the specified artist reference
+        DatabaseReference dR = FirebaseDatabase.getInstance().getReference("artists").child(id);
 
-    private void PrintingData() {
-        macro adapter=new macro(MacroActivity.this, Namelists);
-        lvListview.setAdapter(adapter);
+        //removing artist
+        dR.removeValue();
+
+        //getting the tracks reference for the specified artist
+        DatabaseReference drTracks = FirebaseDatabase.getInstance().getReference("tracks").child(id);
+
+        //removing all tracks
+        drTracks.removeValue();
+        Toast.makeText(getApplicationContext(), "상용구가 삭제되었습니다.", Toast.LENGTH_LONG).show();
+
+        return true;
     }
 
     @Override
-    public void onClick(View v) {
-        switch(v.getId()){
-            case R.id.btnSave:
-                String name=NewName.getText().toString();
-                Namelists.add(name);
-                NewName.setText("");
+    protected void onStart() {
+        super.onStart();
+        //attaching value event listener
+        databaseArtists.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
 
-                SharedPreferences.Editor edito=preference.edit();
-                Number=Number+1;
-                edito.putInt("Number",Number);
-                edito.commit();
+                //clearing the previous artist list
+                artists.clear();
 
-                StoringDataintoSharedfile();
-                PrintingData();
+                //iterating through all the nodes
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                    //getting artist
+                    macro artist = postSnapshot.getValue(macro.class);
+                    //adding artist to the list
+                    artists.add(artist);
+                }
 
-                break;
+                //creating adapter
+                MacroList artistAdapter = new MacroList(MacroActivity.this, artists);
+                //attaching adapter to the listview
+                listViewArtists.setAdapter(artistAdapter);
+            }
 
-            case R.id.btnGoBack:
-               finish();
-        }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
 
+            }
+        });
     }
 
+
+    /*
+    * This method is saving a new artist to the
+    * Firebase Realtime Database
+    * */
+    private void addArtist() {
+        //getting the values to save
+        String name = editTextName.getText().toString().trim();
+      //  String genre = spinnerGenre.getSelectedItem().toString();
+
+        //checking if the value is provided
+        if (!TextUtils.isEmpty(name)) {
+
+            //getting a unique id using push().getKey() method
+            //it will create a unique id and we will use it as the Primary Key for our Artist
+            String id = databaseArtists.push().getKey();
+
+            //creating an Artist Object
+            macro artist = new macro(id, name);
+
+            //Saving the Artist
+            databaseArtists.child(id).setValue(artist);
+
+            //setting edittext to blank again
+            editTextName.setText("");
+
+            //displaying a success toast
+            Toast.makeText(this, "상용구가 추가되었습니다.", Toast.LENGTH_LONG).show();
+        } else {
+            //if the value is not given displaying a toast
+            Toast.makeText(this, "내용을 입력해 주세요.", Toast.LENGTH_LONG).show();
+        }
+    }
 }
+
---
+++ app/src/main/java/com/example/junseo/test03/MacroList.java
@@ -0,0 +1,42 @@
+package com.example.junseo.test03;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Belal on 2/26/2017.
+ */
+
+public class MacroList extends ArrayAdapter<macro> {
+    private Activity context;
+    List<macro> artists;
+
+    public MacroList(Activity context, List<macro> artists) {
+        super(context, R.layout.layout_artist_list, artists);
+        this.context = context;
+        this.artists = artists;
+    }
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+        View listViewItem = inflater.inflate(R.layout.layout_artist_list, null, true);
+
+        TextView textViewName = (TextView) listViewItem.findViewById(R.id.textViewName);
+        TextView textViewGenre = (TextView) listViewItem.findViewById(R.id.textViewGenre);
+
+        macro artist = artists.get(position);
+        textViewName.setText(artist.getArtistName());
+       // textViewGenre.setText(artist.getArtistGenre());
+
+        return listViewItem;
+    }
+}
