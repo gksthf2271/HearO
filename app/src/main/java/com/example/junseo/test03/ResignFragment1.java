6c47c074b44425463fd34148cba5dc1f884d7085
riosutenheat <l035833@naver.com>
2017-09-15 15:51:20 +0900

    //0915 login fix

--- app/src/main/java/com/example/junseo/test03/ResignFragment1.java
+++ app/src/main/java/com/example/junseo/test03/ResignFragment1.java
@@ -1,6 +1,5 @@
 package com.example.junseo.test03;
 
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -19,8 +18,6 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
@@ -39,9 +36,9 @@ public class ResignFragment1 extends Fragment {
     private EditText reetPassword;
     private EditText reetPasswordConfirm;
     private TextView myEmail;
-   // private String name;
+    // private String name;
     private String email;
-//    private String password;
+    //    private String password;
     FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
     final FirebaseUser user = firebaseAuth.getCurrentUser();
     @Nullable
@@ -92,8 +89,8 @@ public class ResignFragment1 extends Fragment {
         reDone.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               final String name = etName.getText().toString();
-                 email = myEmail.getText().toString();
+                final String name = etName.getText().toString();
+                email = myEmail.getText().toString();
                 final String password = reetPassword.getText().toString();
                 if (name.length() == 0) {
                     Toast.makeText(getActivity(), "이름을 입력하세요!", Toast.LENGTH_SHORT).show();
@@ -137,18 +134,30 @@ public class ResignFragment1 extends Fragment {
                     }
                 });
 
-                user.updatePassword(password).addOnCompleteListener(getActivity(),new OnCompleteListener<Void>(){
+                /*user.updatePassword(password).addOnCompleteListener(getActivity(),new OnCompleteListener<Void>(){
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
                         if (task.isSuccessful()) {
                             Toast.makeText(getActivity(), "패스워드가 성공적으로 바뀌었습니다!", Toast.LENGTH_SHORT).show();
                             Log.e("패스워드 성공1","test:");
-                        } /*else {
-                            Toast.makeText(getActivity(), "패스워드를 변경하는데 실패하였습니다.", Toast.LENGTH_SHORT).show();
-                            Log.e("패스워드 실패","test:");
-                        }*/
-                        Log.e("패스워드 성공2","test:" + password );                    }
-                });
+                        }
+                        Log.e("패스워드 성공2","test:" + password );
+
+
+                    }
+                }
+                );*/
+
+                user.updatePassword(password)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Log.e("패스워드 성공2","test:" + password );
+
+                                }
+                            }
+                        });
             }
         });
         //reCancel.setOnClickListener();
